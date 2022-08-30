9dd04efebac448cc700937809a32da74eadb98c9
Ron Pomper <ron.pomper@gmail.com>
2022-08-30 17:58:50 -0400

    update preload csv
    add images

--- stimuli/sounds/preload_audio_list.R
+++ stimuli/sounds/preload_audio_list.R
@@ -2,38 +2,48 @@
 
 setwd("~/Documents/GitHub/F32/stimuli/sounds/teaching")
 cur_sounds = list.files()
-export = ""
+all_sounds = ""
 for (i in 1:length(cur_sounds)) {
-  if (i < length(cur_sounds)) {
-    export = paste(export,"stimuli/sounds/teaching/",cur_sounds[i],",",sep="")
-  } else {
-    export = paste(export,"stimuli/sounds/teaching/",cur_sounds[i],sep="")
-    }
+  all_sounds = paste(all_sounds,"stimuli/sounds/teaching/",cur_sounds[i],",",sep="")
 }
 
 
 setwd("~/Documents/GitHub/F32/stimuli/sounds/dot")
 cur_sounds = list.files()
 for (i in 1:length(cur_sounds)) {
-  export = paste(export,"stimuli/sounds/dot/",cur_sounds[i],",",sep="")
+  all_sounds = paste(all_sounds,"stimuli/sounds/dot/",cur_sounds[i],",",sep="")
 }
 
 
 setwd("~/Documents/GitHub/F32/stimuli/sounds/link")
 cur_sounds = list.files()
 for (i in 1:length(cur_sounds)) {
-  export = paste(export,"stimuli/sounds/link/",cur_sounds[i],",",sep="")
+  all_sounds = paste(all_sounds,"stimuli/sounds/link/",cur_sounds[i],",",sep="")
 }
 
 
 setwd("~/Documents/GitHub/F32/stimuli/sounds/LD")
 cur_sounds = list.files()
 for (i in 1:length(cur_sounds)) {
-  export = paste(export,"stimuli/sounds/LD/",cur_sounds[i],",",sep="")
+  if (i < length(cur_sounds)) {
+    all_sounds = paste(all_sounds,"stimuli/sounds/LD/",cur_sounds[i],",",sep="")
+  } else {
+    all_sounds = paste(all_sounds,"stimuli/sounds/LD/",cur_sounds[i],sep="")
+  }
+}
+
+setwd("~/Documents/GitHub/F32/stimuli/images/")
+cur_images = list.files()
+all_images = ""
+for (i in 1:length(cur_images)) {
+  if (i < length(cur_images)) {
+    all_images = paste(all_images,"stimuli/images/",cur_images[i],",",sep="")
+  } else {
+    all_images = paste(all_images,"stimuli/images/",cur_images[i],sep="")
+  }
 }
 
 
-
 setwd("~/Documents/GitHub/F32/orders/")
-save = data.frame('audio'=export,'images'="")
+save = data.frame('audio'=all_sounds,'images'=all_images,row.names=F)
 write.csv(save,file='F32-preload.csv')
