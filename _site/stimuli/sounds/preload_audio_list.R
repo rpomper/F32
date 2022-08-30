5dd2beadd767c3e9fd3ce120ae4a3c4a54633c51
Ron Pomper <ron.pomper@gmail.com>
2022-08-30 17:34:22 -0400

    update preload csv
    remove spaces between items in array

--- _site/stimuli/sounds/preload_audio_list.R
+++ _site/stimuli/sounds/preload_audio_list.R
@@ -1,39 +1,34 @@
 
 
-setwd("~/Documents/GitHub/F32/stimuli/sounds/dot")
+setwd("~/Documents/GitHub/F32/stimuli/sounds/teaching")
 cur_sounds = list.files()
 export = ""
 for (i in 1:length(cur_sounds)) {
-  export = paste(export,"stimuli/sounds/dot/",cur_sounds[i],", ",sep="")
+  export = paste(export,"stimuli/sounds/teaching/",cur_sounds[i],",",sep="")
 }
 
+
+setwd("~/Documents/GitHub/F32/stimuli/sounds/dot")
+cur_sounds = list.files()
+for (i in 1:length(cur_sounds)) {
+  export = paste(export,"stimuli/sounds/dot/",cur_sounds[i],",",sep="")
+}
+
+
+setwd("~/Documents/GitHub/F32/stimuli/sounds/link")
+cur_sounds = list.files()
+for (i in 1:length(cur_sounds)) {
+  export = paste(export,"stimuli/sounds/link/",cur_sounds[i],",",sep="")
+}
 
 
 setwd("~/Documents/GitHub/F32/stimuli/sounds/LD")
 cur_sounds = list.files()
 for (i in 1:length(cur_sounds)) {
-  export = paste(export,"stimuli/sounds/LD/",cur_sounds[i],", ",sep="")
+  export = paste(export,"stimuli/sounds/LD/",cur_sounds[i],",",sep="")
 }
 
-setwd("~/Documents/GitHub/F32/stimuli/sounds/link")
-cur_sounds = list.files()
-all_sounds = c(all_sounds,cur_sounds)
 
-setwd("~/Documents/GitHub/F32/stimuli/sounds/teaching")
-cur_sounds = list.files()
-all_sounds = c(all_sounds,cur_sounds)
-
-temp = all_sounds
-
-export = "["
-
-for (i in 1:length(all_sounds)) {
-  # print(all_sounds[i])
-  if (i < length(all_sounds)){
-    export = c(export,"/stimuli")
-  }
-
-}
 
 setwd("~/Documents/GitHub/F32/orders/")
 save = data.frame('audio'=export,'images'="")
