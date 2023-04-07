0316c1b2ab5fa81c5bd156e24d686311859ad2eb
Ron Pomper <ron.pomper@gmail.com>
2023-04-07 15:18:49 -0400

    new word
    replaced heab with hibe, new orders, new sounds

--- stimuli/sounds/preload_audio_list.R
+++ stimuli/sounds/preload_audio_list.R
@@ -44,6 +44,18 @@ for (i in 1:length(cur_images)) {
 }
 
 
+setwd("~/Documents/GitHub/F32/stimuli/video/")
+cur_videos = list.files()
+all_videos = ""
+for (i in 1:length(cur_videos)) {
+  if (i < length(cur_videos)) {
+    all_videos = paste(all_videos,"stimuli/video/",cur_videos[i],",",sep="")
+  } else {
+    all_videos = paste(all_videos,"stimuli/video/",cur_videos[i],sep="")
+  }
+}
+
+
 setwd("~/Documents/GitHub/F32/orders/")
-save = data.frame('audio'=all_sounds,'images'=all_images)
+save = data.frame('audio'=all_sounds,'images'=all_images,'video'=all_videos)
 write.csv(save,file='F32-preload.csv',row.names=F)
